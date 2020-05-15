7e149aa8bef9a31208b13ccfde25ce295732b365
A. Jensen <jensen.aaro@gmail.com>
2020-05-14 21:59:30 -0500

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'aj-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'ajensen-client';
+}
