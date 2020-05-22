5d9d4cc1021289824cb2cd72b9c4f93c6fb6cb21
A. Jensen <jensen.aaro@gmail.com>
2020-05-22 02:51:27 -0500

    prototype getting the coordinates of view children

--- src/app/timeline/timeline-list.component.ts
+++ src/app/timeline/timeline-list.component.ts
@@ -1,16 +1,42 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterViewChecked, Component, ElementRef, Input, OnDestroy, OnInit, QueryList, ViewChildren } from '@angular/core';
 import { Project } from '../models/project';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import { TimelineProjectComponent } from './timeline-project.component';
 
 @Component({
   selector: 'aj-timeline-list',
   templateUrl: './timeline-list.component.html',
-  styleUrls: ['./timeline-list.component.scss']
+  styleUrls: ['./timeline-list.component.scss'],
 })
-export class TimelineListComponent implements OnInit {
+export class TimelineListComponent implements OnInit, OnDestroy, AfterViewChecked {
   @Input() projects: Observable<Project[]>;
 
-  ngOnInit(): void {
+  @ViewChildren(TimelineProjectComponent, {read: ElementRef}) timelineListElements: QueryList<ElementRef>;
 
+  private dirty: boolean;
+  private projectsSub: Subscription;
+
+  ngOnInit(): void {
+    this.projectsSub = this.projects
+        .pipe(tap(() => this.dirty = true))
+        .subscribe();
+  }
+
+  ngOnDestroy(): void {
+    this.projectsSub.unsubscribe();
+  }
+
+  ngAfterViewChecked(): void {
+    if ( !this.dirty ) { return; }
+    this.dirty = false;
+
+    for (const p of this.timelineListElements) {
+      if (!p?.nativeElement) {
+        continue;
+      }
+      const dy = p.nativeElement.offsetHeight;
+      console.log(dy);
+    }
   }
 }
